5a924c92739ebacae3a1f013f79b7ac3a74aa5df
Peter Slattery <slattery.peter.j@gmail.com>
2023-10-19 21:35:49 -0700

    Updates

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,12 @@
 import * as vscode from 'vscode';
 
-type KeyboardMode = 
+type KeyboardMode =
  | "input"
  | "command"
 
 let currentModeIndex = -1
+let cachedModeIndex = -1
+
 const modeCycle: KeyboardMode[] = [
 	"input",
 	"command"
@@ -13,21 +15,45 @@ const modeDisplayStringLut: Record<KeyboardMode, string> = {
 	input: "INPUT",
 	command: "COMMAND"
 }
+const modeCursorColor: Record<KeyboardMode, string> = {
+	input: "#00FF00",
+	command: "#FF0000",
+}
 const statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 1)
 
-const activateMode = (modeIndex: number) => {
+
+
+const activateMode = async (modeIndex: number) => {
 	currentModeIndex = modeIndex
 	const mode = modeCycle[currentModeIndex]
 	vscode.commands.executeCommand('setContext', 'gs-vscode-modal-bindings.mode', mode);
 	statusBarItem.text = modeDisplayStringLut[mode]
 	statusBarItem.show();
+	console.log("!!!")
+	await vscode.workspace.getConfiguration().update('workbench.colorCustomizations', {
+		"editorCursor.foreground": modeCursorColor[mode]
+	})
 }
 
-const activateInputMode   = () => activateMode(0)
-const activateCommandMode = () => activateMode(1)
+const activateModeCacheCurrent = async (modeIndex: number) => {
+	cachedModeIndex = currentModeIndex
+	await activateMode(modeIndex)
+}
+const activateCachedMode = async () => {
+	if (cachedModeIndex !== -1) {
+		await activateMode(cachedModeIndex)
+	}
+	cachedModeIndex = -1
+}
 
-const switchModes = () => {
-	activateMode((currentModeIndex + 1) % modeCycle.length)
+
+const activateInputMode   = async () => activateMode(0)
+const activateCommandMode = async () => activateMode(1)
+const activateInputModeCacheCurrent   = async () => activateModeCacheCurrent(0)
+const activateCommandModeCacheCurrent = async () => activateModeCacheCurrent(1)
+
+const switchModes = async () => {
+	await activateMode((currentModeIndex + 1) % modeCycle.length)
 }
 
 export function activate(context: vscode.ExtensionContext) {
@@ -35,6 +61,9 @@ export function activate(context: vscode.ExtensionContext) {
 		vscode.commands.registerCommand('gs-vscode-modal-bindings.switchInputMode', switchModes),
 		vscode.commands.registerCommand('gs-vscode-modal-bindings.activateCommandMode', activateCommandMode),
 		vscode.commands.registerCommand('gs-vscode-modal-bindings.activateInputMode', activateInputMode),
+		vscode.commands.registerCommand('gs-vscode-modal-bindings.activateInputModeCacheCurrent', activateInputModeCacheCurrent),
+		vscode.commands.registerCommand('gs-vscode-modal-bindings.activateCommandModeCacheCurrent', activateCommandModeCacheCurrent),
+		vscode.commands.registerCommand('gs-vscode-modal-bindings.activateCachedMode', activateCachedMode)
 	]
 	disposables.forEach((d) => context.subscriptions.push(d))
 
